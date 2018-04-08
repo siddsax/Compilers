84d375e4a09e1a207fdc26384cb71f36b56457fa
Soumye <soumye@iitk.ac.in>
2018-04-08 19:42:56 +0530

    div_testcases

---
+++ asgn4/div_test/for.cs
@@ -0,0 +1,16 @@
+namespace HelloWorld
+{
+	class Hello 
+	{
+		int Main() 
+		{
+			int i = 6;
+			char a;
+			for (i = 6;i < 9 && i > 5 && i != 7; ++i)
+			{
+				Writeline(i);
+			}
+			return 0;
+		}
+	}
+}
---
+++ asgn4/div_test/test2.cs
@@ -0,0 +1,20 @@
+namespace Program {
+    class Program {
+        int Main() {
+            int num;
+            for(int i = 0;i<10;i = i + 1) {
+                num = num + 1;
+                for (int j = 10; j > 1; j = j -1) {
+                    int k = 0;
+                    k = k + 1;
+                }
+                while(num < 100){
+                    num = num * 10;
+                    while( i > 2) {
+                        i = i - 1;
+                    }
+                }
+            }
+        }
+    }
+}
